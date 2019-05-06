c76d9d0ed9eee05de4cbfc2087d457d185d47838
DESKTOP-T43RPFO\Lee <2353383120@qq.com>
2019-05-06 16:39:11 +0800

    这是个头部组件

---
+++ vue/src/components/LmVueheader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="lheader">
+        <router-link>首页</router-link>
+        <router-link>关于</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LmVueheader"
+    }
+</script>
+
+<style scoped>
+
+</style>
